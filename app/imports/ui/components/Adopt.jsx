5cbd633377e55a55342bfddf90018914a474f949
maeganmc <maeganmc@users.noreply.github.com>
2021-04-24 22:29:20 -0400

    Adopt-a-Cat page

---
+++ app/imports/ui/components/Adopt.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Adopt extends React.Component {
+  render() {
+    return (
+      <Card>
+        <Image
+          size='large'
+          src={this.props.adopt.image}
+        />
+        <Card.Content>
+          <Card.Header>{this.props.adopt.name}</Card.Header>
+          <Card.Meta>({this.props.adopt.gender})</Card.Meta>
+          <Card.Description>
+            {this.props.adopt.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button>
+              Adopt
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+Adopt.propTypes = {
+  adopt: PropTypes.object.isRequired,
+  notes: PropTypes.array.isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(Adopt);
